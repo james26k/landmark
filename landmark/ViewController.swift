32a5677c9b0f2333243b1b5e183d7d344203c673
james26k <cb26.hayashi@gmail.com>
2021-09-10 00:01:58 +0900

    chore: create project

---
+++ landmark/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  landmark
+//
+//  Created by Kohei Hayashi on 2021/09/09.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
